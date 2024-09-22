eb51ec080defeeb4fb75335387e4fdecd38a742c
sai4coding <155803032+sai4coding@users.noreply.github.com>
2024-09-22 17:54:07 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,30 @@
+//alert("hello")
+let a=10
+console.log(a)
+let b=2.4
+console.log(b)
+let condition=true
+console.log(condition)
+let name="sai"
+console.log(name)
+ a=12
+ console.log(a)
+ let obj={name:"sai"}
+ console.log(obj)
+ console.log(typeof(obj))
+let h=null
+console.log(typeof(h))
+
+h="hehe"
+console.log(h)
+// alert(h)
+//let val=prompt("enter age")
+//console.log(val)
+
+//console.log(prompt(a+b))
+let st="nagasai"
+console.log(st.length)
+console.log(st.charAt(2))
+console.log(st.charAt(4))
+console.log(st.slice(1,4))
+console.log(st.substr(1,4))
